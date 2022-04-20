46da6cca193da68eb1d69bbd724fae45c58652c4
Aman Jat <aman@discoverwebtech.com>
2022-04-20 15:20:18 +0530

    update

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,6 +4,7 @@ import { CID, IPFSHTTPClient } from "ipfs-http-client";
 import getWeb3 from "./getWeb3";
 import ipfs from "./ipfs";
 import "./App.css";
+import ABI from './MyNft.json'
 
 const App = () => {
   const [storageValue, setStorageValue] = useState<number>(5);
@@ -11,13 +12,23 @@ const App = () => {
   const [accounts, setAccounts] = useState();
   const [contract, setContract] = useState();
   const [images, setImages] = useState<{ cid: CID; path: string }[]>([]);
+  const ADDRESS = '0xCA79577D4767A968F7Daf8b19d9BAE4997E5d75b';
+
+
+  const handleMintNFT = async () => {
+    console.log("minting start...");
+    if(accounts && contract) {
+    console.log(accounts[0]);
+      (contract as any).methods.mintNFT(accounts[0]).send({ from: accounts[0], value: "100000000000000000" });
+    }
+  };
 
   const runExample = async () => {
     const web3 = await getWeb3();
     const accounts = await web3.eth.getAccounts();
     const networkId = await web3.eth.net.getId();
     const response = await (contract as any).methods.get().call();
-    await (contract as any).methods.set(10).send({ from: accounts[0] });
+    // await (contract as any).methods.set(10).send({ from: accounts[0] });
     setAccounts(accounts[0]);
     setStorageValue(response);
   };
@@ -30,11 +41,8 @@ const App = () => {
       return alert("No files selected");
     }
     const filesAsArray = Array.from(files);
-    console.log("filesAsArray", filesAsArray);
     filesAsArray.forEach(async (file: any) => {
-      console.log("file in loop", file);
       const result = await (ipfs as IPFSHTTPClient).add(file);
-      console.log("result", result);
       setImages([
         ...images,
         {
@@ -42,9 +50,7 @@ const App = () => {
           path: result.path,
         },
       ]);
-      console.log("images :", images);
     });
-    console.log("images after :", images);
   };
 
   useEffect(() => {
@@ -54,7 +60,7 @@ const App = () => {
         const accounts = await web3.eth.getAccounts();
         const networkId = await web3.eth.net.getId();
         const deployedNetwork = (SimpleStorageContract as any).networks[networkId];
-        const instance = new web3.eth.Contract(SimpleStorageContract.abi, deployedNetwork && deployedNetwork.address);
+        const instance = new web3.eth.Contract(ABI, ADDRESS);
         setWeb3(web3);
         setAccounts(accounts);
         setContract(instance);
@@ -71,16 +77,8 @@ const App = () => {
     return <div>Loading Web3, accounts, and contract...</div>;
   }
 
-  const handleMintNFT = () => {
-    console.log("minting start...");
-    if (!contract) {
-      console.log("contract is not available --contract-", contract);
-    } else {
-      console.log("contract found --contract-", contract);
-      // contract.methods.safeMint(account).send({ from: accounts, value: "10000000000000000000" });
-    }
-  };
-  // 0x26cd2bae73293c77891d4436c057dfef3a375b8d;
+// do not remove this line
+// 0xd9145CCE52D386f254917e481eB44e9943F39138
   return (
     <div className="App">
       {accounts ? <p style={{ color: "green" }}>Connected Account : {accounts}</p> : <p>Connect to Blockchain Network</p>}
