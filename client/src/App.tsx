3d59f725970da63476c4b36ac1dc5bad8341b6c6
Aman Jat <amanjat261@gmail.com>
2022-05-02 11:50:02 +0530

    single image can be uploaded to ipfs

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,9 @@
-import React, { Component, useState, useEffect } from "react";
-import SimpleStorageContract from "./contracts/SimpleStorage.json";
+import React, { useState, useEffect } from "react";
 import { CID, IPFSHTTPClient } from "ipfs-http-client";
 import getWeb3 from "./getWeb3";
 import ipfs from "./ipfs";
 import "./App.css";
-import ABI from './MyNft.json'
+import ABI from "./MyNft.json";
 
 const App = () => {
   const [storageValue, setStorageValue] = useState<number>(5);
@@ -12,13 +11,13 @@ const App = () => {
   const [accounts, setAccounts] = useState();
   const [contract, setContract] = useState();
   const [images, setImages] = useState<{ cid: CID; path: string }[]>([]);
-  const ADDRESS = '0xCA79577D4767A968F7Daf8b19d9BAE4997E5d75b';
-
+  const [generatedHash, setGeneratedHash] = useState<string>();
+  const ADDRESS = "0xCA79577D4767A968F7Daf8b19d9BAE4997E5d75b";
 
   const handleMintNFT = async () => {
     console.log("minting start...");
-    if(accounts && contract) {
-    console.log(accounts[0]);
+    if (accounts && contract) {
+      console.log(accounts[0]);
       (contract as any).methods.mintNFT(accounts[0]).send({ from: accounts[0], value: "100000000000000000" });
     }
   };
@@ -26,7 +25,6 @@ const App = () => {
   const runExample = async () => {
     const web3 = await getWeb3();
     const accounts = await web3.eth.getAccounts();
-    const networkId = await web3.eth.net.getId();
     const response = await (contract as any).methods.get().call();
     // await (contract as any).methods.set(10).send({ from: accounts[0] });
     setAccounts(accounts[0]);
@@ -37,12 +35,16 @@ const App = () => {
     e.preventDefault();
     const form = e.target as HTMLFormElement;
     const files = (form[0] as HTMLInputElement).files;
+    console.log("files", files);
     if (!files || files.length === 0) {
       return alert("No files selected");
     }
     const filesAsArray = Array.from(files);
-    filesAsArray.forEach(async (file: any) => {
+    filesAsArray.forEach(async (file: any, index: number) => {
       const result = await (ipfs as IPFSHTTPClient).add(file);
+      console.log("results-cid : ", result.cid);
+      console.log("results-path : ", result.path);
+
       setImages([
         ...images,
         {
@@ -50,6 +52,8 @@ const App = () => {
           path: result.path,
         },
       ]);
+
+      setGeneratedHash(result.path);
     });
   };
 
@@ -58,8 +62,6 @@ const App = () => {
       try {
         const web3 = await getWeb3();
         const accounts = await web3.eth.getAccounts();
-        const networkId = await web3.eth.net.getId();
-        const deployedNetwork = (SimpleStorageContract as any).networks[networkId];
         const instance = new web3.eth.Contract(ABI, ADDRESS);
         setWeb3(web3);
         setAccounts(accounts);
@@ -77,29 +79,27 @@ const App = () => {
     return <div>Loading Web3, accounts, and contract...</div>;
   }
 
-// do not remove this line
-// 0xd9145CCE52D386f254917e481eB44e9943F39138
+  // do not remove this line
+  // 0xd9145CCE52D386f254917e481eB44e9943F39138
   return (
     <div className="App">
       {accounts ? <p style={{ color: "green" }}>Connected Account : {accounts}</p> : <p>Connect to Blockchain Network</p>}
-
       <div>Stored value: {storageValue}</div>
-
       <h1>IPFS Upload Files DApp</h1>
-
       <p>This image is uploaded on IPFS and etherium blockchain</p>
-
       <form onSubmit={onSubmit}>
-        <input type="file" multiple />
-        <input type="submit" />
+        <input type="file" />
+        <input type="submit" className="button-62" />
       </form>
       {images.length ? <p style={{ color: "green" }}>{images.length} files have been uploaded successfully</p> : <p>No files have been uploaded</p>}
-      {images.length ? images.map((image, index) => <img height={250} width={250} key={image.path} src={`https://ipfs.io/ifps/${image.path}`} alt={`Uploaded #${index + 1}`} />) : null}
+      {images.length ? images.map((image, index) => <img height={250} width={250} key={image.path} src={`https://ipfs.io/ipfs/${image.path}`} alt={`Uploaded #${index + 1}`} />) : null}
       <br />
+      {generatedHash && <p>Genearted Hash : {generatedHash}</p>}
       <p>-------------------------------------------------</p>
       <br />
-
-      <button onClick={handleMintNFT}>MINT NFT</button>
+      <button onClick={handleMintNFT} className="button-62">
+        MINT NFT
+      </button>
     </div>
   );
 };
